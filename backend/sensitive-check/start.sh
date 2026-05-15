cdc2db198d55a980d2ac24b83aa9ba3c746cd140
Zmjjeff7 <2519416535@qq.com>
2026-05-16 02:18:12 +0800

    Revert "fear: add newest backend code"
    This reverts commit 383b5acb8219dbbc61a239cf6e4c1d7f86994944.

--- backend/sensitive-check/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /opt/itsmappin/Backend_ItsMapPin/sensitive-check
-set -a
-source .env
-set +a
-echo "BIC_API_KEY: ${BIC_API_KEY:0:10}..."  # 调试输出
-echo "BIC_SECRET_KEY: ${BIC_SECRET_KEY:0:10}..."  # 调试输出
-exec /opt/itsmappin/Backend_ItsMapPin/.venv/bin/python baidu_image_censor.py --host 127.0.0.1 --port 9200
